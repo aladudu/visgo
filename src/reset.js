07b5293f779afee7ccf4d98f8fa740b8cb29ecc1
Your Name <you@example.com>
2019-04-20 08:57:36 +0800

    visgo 2.0.0

--- src/reset.js
+++ src/reset.js
@@ -1,4 +1,4 @@
-import {createGlobalStyle } from "styled-components";
+import {createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
 html, body, ul, li, ol, dl, dd, dt, p, h1, h2, h3, h4, h5, h6, form, fieldset, legend, img { margin:0; padding:0; }
